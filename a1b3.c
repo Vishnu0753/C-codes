c36cbf0abdb8adbfc2d0c89a65164c3c02397e8e
Vishnu0753 <87539698+Vishnu0753@users.noreply.github.com>
2022-08-03 18:45:38 +0530

    Add files via upload

---
+++ a1b3.c
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main(void)
+{
+char str[100],last;
+int l=0,i,n=0,arr[l],k=0;
+printf("Enter the string  : ");
+scanf("%s",str);
+l=strlen(str);
+for(i=0;i<l;i++)
+{
+  if(str[i]>=48 || str[i]<=57)
+  {
+    k = (int)str[i+1] - 48;
+    for(int j=0; j<k;j++){
+      printf("%c",str[i]);
+  }
+  }
+  i++;
+}
+
+}
